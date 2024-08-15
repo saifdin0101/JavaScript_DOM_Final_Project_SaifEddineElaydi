eff17ca6e7cb749c2736d312bcec525a4a665d0b
saifdin0101 <saifdin0808@gmail.com>
2024-08-15 16:46:04 +0100

    first coroseul

--- public/js/app.js
+++ public/js/app.js
@@ -96,3 +96,71 @@ login.addEventListener("click", () => {
 
     });
 });
+
+
+
+
+
+
+
+let nextBtns = document.querySelectorAll(".next")
+let previousBtns = document.querySelectorAll(".previous")
+let containers = document.querySelectorAll(".carousel-container")
+
+let currentIndex = 0
+const slideImage = (index, myBtn) => {
+  let carouselBtnAttribute = myBtn.getAttribute("carouselBtn")
+  containers.forEach(container => {
+    if (container.id == carouselBtnAttribute) {
+      let slides = container.querySelectorAll(".slide")
+      let camera = container.querySelector(".carousel")
+      let slideWidth = slides[0].clientWidth
+      let indicators = container.querySelectorAll('.indicator')
+
+      if (index < 0) {
+        index = slides.length - 1
+      } else if (index >= slides.length) {
+        index = 0
+      }
+
+      indicators.forEach(indicator => {
+        indicator.classList.remove('activeIndicator')
+      });
+      indicators[index].classList.add("activeIndicator")
+
+      camera.style.transform = `translateX(-${slideWidth * index}px)`
+      currentIndex = index
+    }
+  });
+}
+
+nextBtns.forEach(next => {
+  next.addEventListener("click", (event) => { slideImage(currentIndex + 1, event.target) })
+});
+
+previousBtns.forEach(previous => {
+  previous.addEventListener("click", (event) => { slideImage(currentIndex - 1, event.target) })
+});
+
+containers.forEach(container => {
+  if (container.getAttribute("autoslide")) {
+    let nextBtn = container.querySelector(".next")
+    setInterval(() => {
+      nextBtn.click()
+    }, 5000); 
+  }
+});
+
+containers.forEach(container => {
+  let slides = container.querySelectorAll(".slide")
+  let indicatorsGrp = document.createElement("div")
+  indicatorsGrp.classList.add("indicators-grp")
+  container.appendChild(indicatorsGrp)
+
+  slides.forEach(slide => {
+    let indicator = document.createElement("div")
+    indicator.classList.add("indicator")
+    indicatorsGrp.appendChild(indicator)
+  });
+  indicatorsGrp.querySelector(".indicator").classList.add('activeIndicator')
+});
