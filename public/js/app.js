ae429ef76cb6a541072b204ea693f615f5ea4234
saifdin0101 <saifdin0808@gmail.com>
2024-08-15 15:13:49 +0100

    login  modal

--- public/js/app.js
+++ public/js/app.js
@@ -9,3 +9,90 @@ playButton.addEventListener("click", ()=> {
 
 
 });
+
+
+
+
+let imgg = document.querySelectorAll(".imgg")
+let starTers = document.querySelector(".click-1")
+let breakfast = document.querySelector(".click-2")
+let lunch = document.querySelector(".click-3")
+let dinner = document.querySelector(".click-4")
+let hh = document.querySelector(".verybig")
+
+breakfast.addEventListener("click",()=>{
+    hh.textContent="breakfast"
+    
+    imgg.forEach(element => {
+        
+        element.setAttribute("src","./public/images/menu-item-2.png")
+        
+    });
+
+})
+lunch.addEventListener("click",()=>{
+    hh.textContent="Lunch"
+    imgg.forEach(element => {
+        element.setAttribute("src","./public/images/menu-item-3.png")
+    });
+
+})
+dinner.addEventListener("click",()=>{
+    hh.textContent="Dinner"
+    imgg.forEach(element => {
+        element.setAttribute("src","./public/images/menu-item-4.png")
+    });
+
+})
+starTers.addEventListener("click",()=>{
+    hh.textContent="Starters"
+    imgg.forEach(element => {
+        element.setAttribute("src","./public/images/menu-item-1.png")
+    });
+
+})
+
+
+
+let login = document.querySelector(".log");
+let mybody = document.body;
+let mdl = document.querySelector(".hohoho");
+let backdrop = document.createElement("div"); 
+backdrop.className = "backdrop";
+
+document.body.appendChild(backdrop); 
+
+login.addEventListener("click", () => {
+    mdl.innerHTML = `
+        <div class="modal">
+            <h1>Log In Section</h1>
+            <div>
+                <label for="">Username&#160;</label>
+                <input placeholder="&#160;&#160;&#160;Type Your Username" type="text">
+            </div>
+            <div>
+                <label for="">Password&#160;&#160;</label>
+                <input placeholder="&#160;&#160;&#160;&#160;Type your Password" type="password">
+            </div>
+            <div>
+                <label for="">Email&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;</label>
+                <input placeholder="&#160;&#160;&#160;Type Your Email" type="email">
+            </div>
+            <button class="save">Send</button>
+        </div>
+    `;
+    
+    backdrop.style.display = "block"; 
+    login.style.display = "none";
+    mdl.style.position="relative"
+    mdl.style.top="100px"
+    
+    
+    let sent = document.querySelector(".save");
+    sent.addEventListener("click", () => {
+        mdl.style.display = "none";
+        login.style.display = "block";
+        backdrop.style.display = "none"; 
+
+    });
+});
